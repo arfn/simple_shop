c12e032364f3da9b33875704a7e097300331d517
arifin <arifingunawan96@gmail.com>
2015-07-17 22:56:55 +0700

    Register page without Password hashing

--- customer/register.php
+++ customer/register.php
@@ -1,7 +1,20 @@
+<?php include '../model/customer.php'; 
+	$model = new Customer_Model();
+?>
 <?php 
-	if(isset($_POST['customer_register_submit']))
+
+	//TODO password hashing
+	if(isset($_POST['customer_submit']))
 	{
-		
+		$args = array(
+			'customer_name' => FILTER_SANITIZE_SPECIAL_CHARS,
+			'customer_username' => FILTER_SANITIZE_SPECIAL_CHARS,
+			'customer_password' => FILTER_SANITIZE_SPECIAL_CHARS,
+			'customer_email' => FILTER_VALIDATE_EMAIL
+			);
+
+        $input = filter_input_array(INPUT_POST, $args);		
+		$model->register_customer($input);
 	}
 	else{
 ?>
@@ -16,25 +29,25 @@
 	<div class="register_form_wrapper">
 	<div class="form_ribbon">CUSTOMER REGISTRATION</div>
 		<form method="POST" action="">
-			<label for="customer_register_name">Full Name</label>
-			<input type="text" name="customer_register_name" placeholder="John Doe" required="required">
+			<label for="customer_name">Full Name</label>
+			<input type="text" name="customer_name" placeholder="John Doe" required="required">
 			<br>
-			<label for="customer_register_username">Username</label>
-			<input type="text" name="customer_register_username" required="required">
+			<label for="customer_username">Username</label>
+			<input type="text" name="customer_username" required="required">
 			<br>
-			<label for="customer_register_email">E-mail</label>
-			<input type="email" name="customer_register_email" required="required">
+			<label for="customer_email">E-mail</label>
+			<input type="email" name="customer_email" required="required">
 			<br>
-			<label for="customer_register_password">Password</label>
-			<input type="password" name="customer_register_password" required="required">
+			<label for="customer_password">Password</label>
+			<input type="password" name="customer_password" required="required">
 			<br>
-			<label for="customer_register_password_confirmation">Password Confirmation</label>
-			<input type="password" name="customer_register_password_confirmation" required="required">
+			<label for="customer_password_confirmation">Password Confirmation</label>
+			<input type="password" name="customer_password_confirmation" required="required">
 			<br>
-			<input type="submit" value="Register" name="customer_register_submit">
+			<input type="submit" value="Register" name="customer_submit">
 		</form>
 	</div>
 </body>
 </html>
-
+<?php include '../templates/footer.php';?>
 <?php } ?>
---
+++ model/customer.php
@@ -0,0 +1,27 @@
+<?php 
+include '../config/database.php';
+
+class Customer_Model extends DatabaseConnection
+{
+	public function register_customer($data = array())
+	{
+		function add_quote($str)
+          {
+              return sprintf("'%s'", $str);
+          }
+
+          $column = implode(", ", array_keys($data));
+          $values = implode(", ", array_map("add_quote", array_values($data)));
+          $sql = $this->dbh->prepare("INSERT INTO customer ($column) VALUES ($values);");
+
+          if ($sql->execute())
+          {
+            echo "<div class='notice_redirect'><p>Successfuly Register. Please check your email confirmation.</p></div>";
+          }
+          else {
+            echo "<div class='notice_redirect'><p>An error occured while Register</p></div>";
+          }
+	}
+}
+
+ ?>
